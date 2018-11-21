60366f9ea346741e21db4f97ab48b34debb5350c
Victoria <victoriasheng@hotmail.com>
2018-11-21 10:49:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/interfaceone/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.interfaceone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.d("viki", "metodo onStart");
+    }
+
+}
